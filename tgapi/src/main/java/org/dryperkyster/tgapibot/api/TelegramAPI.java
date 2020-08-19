42c07eae0325b92d2ed7bc9bd08b3ea359649e8a
YukoSky <daffetyxd@gmail.com>
2020-08-19 10:07:59 -0300

    tgapi: api: fix wrong mChatId and make check in id/key

--- tgapi/src/main/java/org/dryperkyster/tgapibot/api/TelegramAPI.java
+++ tgapi/src/main/java/org/dryperkyster/tgapibot/api/TelegramAPI.java
@@ -1,23 +1,41 @@
 package org.dryperkyster.tgapibot.api;
 
+import android.app.Activity;
+
 public class TelegramAPI
 {
-	
+
 	/*
 	 * General headers
 	 * param @ mApiKey = Bot's key - You can find in @BotFather
 	 * param @ mChatUd = Chat id, use MissRose or custom client to get id
 	 * param @ mBotUrl = Not a general function for user
 	 */
-	 
+
 	private static String mApiKey = null;
-	private static String mChatId = null;
+	private static Integer mChatId = null;
 	private static String mBotUrl = "https://api.telegram.org/bot" + mApiKey + "/sendMessage";
-	 
-	
-	public TelegramAPI(String mApiKey, String mChatId) {
+	private Activity mActivity;
+
+	public TelegramAPI(String mApiKey, Integer mChatId, Activity mActivity)
+	{
 		this.mApiKey = mApiKey;
 		this.mChatId = mChatId;
+		this.mActivity = mActivity;
+
+		if (mApiKey.equals(null) || mApiKey.equals(""))
+		{
+			throw new NullPointerException("mTelegramLib: Api key null or empty, fix that!");
+		}
+
+		if (mChatId.equals(null) || mChatId.equals(""))
+		{
+			throw new NullPointerException("mTelegramLib: Chat id null or empty, fix that!");
+		}
 	}
-	
+
+	public Activity getActivity()
+	{
+		return this.mActivity;
+	}
 }
