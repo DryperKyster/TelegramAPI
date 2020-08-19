9ecbc13fa14d512e39f34e023ff6fd2afebaba39
YukoSky <daffetyxd@gmail.com>
2020-08-19 12:39:35 -0300

    repo: migrate do Android Studio make format and others in source
    Signed-off-by: YukoSky <daffetyxd@gmail.com>

--- app/src/main/java/br/com/vevelo/telegramapi/ui/MainActivity.java
+++ app/src/main/java/br/com/vevelo/telegramapi/ui/MainActivity.java
@@ -2,43 +2,41 @@ package br.com.vevelo.telegramapi.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import br.com.vevelo.telegramapi.R;
-import org.dryperkyster.tgapibot.api.TelegramAPI;
-import android.view.View;
 
-public class MainActivity extends Activity
-{
-	
-	public EditText mEtToken;
-	public EditText mEtChatId;
-	public EditText mEtMessage;
-	
-	public Button mSendButton;
-	
-	public TelegramAPI tgApi;
-	
+import org.dryperkyster.tgapibot.api.TelegramAPI;
+
+import br.com.vevelo.telegramapi.R;
+
+public class MainActivity extends Activity {
+
+    public EditText mEtToken;
+    public EditText mEtChatId;
+    public EditText mEtMessage;
+
+    public Button mSendButton;
+
+    public TelegramAPI tgApi;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		idLoad();
+        idLoad();
     }
 
-	private void idLoad()
-	{
-		mEtToken = findViewById(R.id.mEtToken);
-		mEtChatId = findViewById(R.id.mEtChatId);
-		mEtMessage = findViewById(R.id.mEtMessage);
-	    mSendButton = findViewById(R.id.mSendButton);
-		mSendButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view)
-			{
-				
-			}
-		});
-	}
+    private void idLoad() {
+        mEtToken = findViewById(R.id.mEtToken);
+        mEtChatId = findViewById(R.id.mEtChatId);
+        mEtMessage = findViewById(R.id.mEtMessage);
+        mSendButton = findViewById(R.id.mSendButton);
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
 }
